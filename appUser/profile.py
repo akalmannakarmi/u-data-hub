0f7e82e5845ffd3301a4b5421460ac43f37a81d9
akal <akalnakarmi1@gmail.com>
2024-02-04 09:56:30 +0545

    user routes

--- appUser/profile.py
+++ appUser/profile.py
@@ -3,26 +3,82 @@ from . import app,db
 
 @app.route('/profile/<userTag>',methods=["GET"])
 def viewProfile(userTag):
-    userId = getUserId(userTag)
-    userData = getUserData(userId)
-    return render_template('viewProfile.html',session=session,userData=userData)
+    userId = db.getUserId(userTag)
+    if userId == session['userId']:
+        userData = db.getUserData(userId,0)
+    else:
+        userData = db.getUserData(userId,session['userId'])
+    return render_template('viewProfile.html',session=session,userTag=userTag,userData=userData)
 
 @app.route('/myProfile',methods=["GET"])
 def myProfile():
-    userData = getUserData(userId)
-    return render_template('myProfile.html',session=session,userData=userData)
+    if 'userId' not in session:
+        return redirect('/login')
+    
+    userTag = db.getUserTag(session['userId'])
+    userKey = db.getUserKey(session['userId'])
+    userData = db.getMyData(session['userId'])
+    return render_template('myProfile.html',session=session,userTag=userTag,userKey=userKey,userData=userData)
+
+@app.route('/myProfile/newKey',methods=["POST"])
+def newKey():
+    if 'userId' not in session:
+        return redirect('/login')
+    
+    db.newKey(session['userId'])
+    return redirect('/myProfile')
 
 @app.route('/myProfile/<category>/add',methods=["POST"])
-def add():
-    userData = getUserData(session['userId'])
+def addFields(category):
+    if 'userId' not in session:
+        return redirect('/login')
+
+    categoriesAndFields= db.getCategoriesAndFields()
+    if category not in categoriesAndFields:
+        return jsonify('Category Does not Exists')
+    
+    data = request.form
+    fields = categoriesAndFields[category]
+    fieldValues = {key: value for key, value in data.items() if key in fields}
+    fieldPrivacy = {key[:-1]: value for key, value in data.items() if key[:-1] in fields}
+    
+    db.addInfo(session['userId'],category,fieldValues,fieldPrivacy)
+
     return redirect('/myProfile')
 
 @app.route('/myProfile/<category>/edit',methods=["POST"])
-def edit():
-    userData = getUserData(session['userId'])
+def editFields(category):
+    if 'userId' not in session:
+        return redirect('/login')
+
+    categoriesAndFields= db.getCategoriesAndFields()
+    if category not in categoriesAndFields:
+        return jsonify('Category Does not Exists')
+    
+    
+    data = request.form
+    fields = categoriesAndFields[category]
+    fieldValues = {key: value for key, value in data.items() if key in fields}
+    fieldPrivacy = {key[:-1]: value for key, value in data.items() if key[:-1] in fields}
+    
+    db.editInfo(session['userId'],category,fieldValues,fieldPrivacy)
+
     return redirect('/myProfile')
 
 @app.route('/myProfile/<category>/remove',methods=["POST"])
-def remove():
-    userData = getUserData(session['userId'])
+def removeFields(category):
+    if 'userId' not in session:
+        return redirect('/login')
+    
+    categoriesAndFields= db.getCategoriesAndFields()
+    if category not in categoriesAndFields:
+        return jsonify('Category Does not Exists')
+
+    
+    data = request.form
+    fields = categoriesAndFields[category]
+    fieldValues = [key for key in data if key in fields]
+    
+    db.removeInfo(session['userId'],category,fieldValues)
+
     return redirect('/myProfile')
--- data/dbUser.py
+++ data/dbUser.py
@@ -1,55 +1,108 @@
 from .data import db,ConnPool,convertValue,toBlob
+from . import dbAPI
 
 class dbUser:
+    def newKey(userId):
+        dbAPI.changeKey(userId)
+    
+    def getCategories():
+        return db.categories
+    
+    def getCategoriesAndFields():
+        return db.categoriesAndFields
+    
     def getUserId(userTag):
-        pass
-
-    def getUserData(userId):
         with ConnPool.getConn() as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT fieldId, value, isPrivate FROM Data WHERE userId=?", (userId,))
+            cursor.execute("SELECT id FROM User WHERE tag=?", (userTag,))
+            result = cursor.fetchone()[0]
+            cursor.close()
+            return result
+    
+    def getUserTag(userId):
+        with ConnPool.getConn() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT tag FROM User WHERE id=?", (userId,))
+            result = cursor.fetchone()[0]
+            cursor.close()
+            return result
+    
+    def getUserKey(userId):
+        with ConnPool.getConn() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT key FROM User WHERE id=?", (userId,))
+            result = cursor.fetchone()[0]
+            cursor.close()
+            return result
+
+    def getUserData(userId,userId2):
+        with ConnPool.getConn() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT dataId FROM Shared WHERE userId=?",(userId2,))
+            dataIds=cursor.fetchall()
+            dataIds=[id[0] for id in dataIds]
+            cursor.execute("SELECT fieldId, value, isPrivate FROM Data WHERE userId=? AND (isPrivate NOT IN (1,2) OR id IN ?)", (userId,dataIds))
             values = cursor.fetchall()
             cursor.close()
+
             result={}
             for fieldId,raw,isPrivate in values:
                 category,field,value = convertValue(fieldId,raw)
                 result[category][field]=(value,isPrivate==1)
             return result
     
-    def addInfo(userId,keyvalues):
+    def getMyData(userId):
         with ConnPool.getConn() as conn:
             cursor = conn.cursor()
-            if any(key not in db.revCategoryAndField for key in keyvalues):
-                raise Exception("Category or Field does not exists")
-            
+            cursor.execute("SELECT fieldId, value, isPrivate FROM Data WHERE userId=?", (userId,))
+            values = cursor.fetchall()
+            cursor.close()
+
+            result={}
+            for fieldId,raw,isPrivate in values:
+                category,field,value = convertValue(fieldId,raw)
+                result[category][field]=(value,isPrivate==1)
+            return result
+    
+    def addInfo(userId,category,fieldValues,fieldPrivacy):
+        with ConnPool.getConn() as conn:
+            cursor = conn.cursor()
             q="INSERT INTO Data (userId,fieldId,valueId,isPrivate) VALUES (?,?,?)"
 
-            for fieldId,(value,isPrivate) in keyvalues.items():
+            for field,value in fieldValues.items():
                 bvalue=toBlob(value)
-                cursor.execute(q,(userId,fieldId,bvalue,isPrivate))
+                fieldId=db.categoriesAndFields[category][field]
+                privacy = fieldPrivacy[field]
+                cursor.execute(q,(userId,fieldId,bvalue,privacy))
             
             conn.commit()
             cursor.close()
 
-    def editInfo(userId,keyvalues):
+    def editInfo(userId,category,fieldValues,fieldPrivacy):
         with ConnPool.getConn() as conn:
             cursor = conn.cursor()
-            if any(key not in db.revCategoryAndField for key in keyvalues):
-                raise Exception("Category or Field does not exists")
             
-            q="UPDATE Data SET value=?, isPrivate=? WHERE userId=? AND fieldId=?"
+            q="UPDATE Data SET value=? isPrivate=? WHERE userId=? AND fieldId=?"
 
-            for fieldId,(value,isPrivate) in keyvalues.items():
+            for field,value in fieldValues.items():
                 bvalue=toBlob(value)
-                cursor.execute(q,(bvalue,isPrivate,userId,fieldId))
+                fieldId=db.categoriesAndFields[category][field]
+                privacy=fieldPrivacy[field]
+                cursor.execute(q,(bvalue,privacy,userId,fieldId))
             
             conn.commit()
             cursor.close()
         
     
-    def removeInfo(userId,keys):
+    def removeInfo(userId,category,keys):
         with ConnPool.getConn() as conn:
             cursor = conn.cursor()
-            cursor.execute("DELETE FROM Data WHERE userId=? AND fieldId IN ?")
+
+            q="DELETE FROM Data WHERE userId=? AND fieldId IN ?"
+            
+            for field in keys:
+                fieldId=db.categoriesAndFields[category][field]
+                cursor.execute(q,(userId,fieldId))
+            
             conn.commit()
             cursor.close()
